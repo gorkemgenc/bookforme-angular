dd5a07d9bdd205cdf1337e783774267e283f8ad9
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-27 07:33:39 +0300

    modify default angular starter project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookforme-angular2';
+  title = 'bookforme-angular';
+
+  clickHandler(){
+    alert('Clicking');
+  }
 }
