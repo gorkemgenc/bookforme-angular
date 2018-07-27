60a8ab1de35a82a14fee0db5de402b6213cbd321
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-27 11:43:16 +0300

    Added rental module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent} from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { TempComponent } from './temp/temp.component';
 
+import { RentalModule } from './rental/rental.module';
+
 const routes: Routes = [
   {path: '', component:RentalComponent},
   {path: 'temp', component:TempComponent}
@@ -16,12 +18,12 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    RentalComponent,
     TempComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
-    BrowserModule
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
