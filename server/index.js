c1303c91931cbda810386ec4269aa20e61b5f2e0
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-29 09:53:56 +0300

    Tested express and connected MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
 const app = express();
+
+mongoose.connect(config.DB_URI, {useNewUrlParser: true});
+
+app.get('/rentals', function(req, res){
+    res.json({
+        'success' : true
+    });
+});
 
 // check if there is PORT or not for environment file
 const PORT = process.env.PORT || 3001;
