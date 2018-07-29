434517909fa4dde3c9ea40a9e6172b3b887401cb
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-29 18:33:03 +0300

    Added Displayed Area of Rental Map

--- src/app/common/map/map.service.ts
+++ src/app/common/map/map.service.ts
@@ -31,7 +31,7 @@ export class MapService {
             this.geoCoder.geocode({address: location}, (result, status) => {
                 if(status === 'OK'){
                     const geometry = result[0].geometry.location;
-                    const coordinates = {lat: geometry.lat(), lng: geometry.lng()};
+                    const coordinates = {lat: parseFloat(geometry.lat()), lng: parseFloat(geometry.lng())};
                     
                     this.cacheLocation(location, coordinates);
                     observer.next(coordinates);
