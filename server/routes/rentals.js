65afa5e1e0e49c327835a594d58dec7b78b82b79
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-29 12:32:51 +0300

    Fixed route problem

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -5,13 +5,22 @@ const Rental = require('../models/rental');
 router.get('', function(req, res){
     Rental.find({}, function(err, foundRentals){
         res.json(foundRentals);
-    })
+    });
 });
 
-router.get('/:id', function(req, res){
+router.get('/:id', function(req, res) { 
     const rentalId = req.params.id;
-    
-    Rental.findById(id)
+
+    Rental.findById(rentalId, function(err, foundRental){
+        if(err){
+            res.status(422).send({
+                errors:[
+                    {title: 'Rental Error', details: 'Could not found Rental'}
+                ]
+            });
+        }
+        res.json(foundRental);
+    });
 });
 
 module.exports = router;
