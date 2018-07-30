dda45523b9e2e15e59d01fbbf30cebf8e3654b80
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-30 18:32:02 +0300

    Added register operation

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,60 @@
+const User = require('../models/user');
+
 exports.auth = function(req, res){
 
 }
 
 exports.register = function(req, res){
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const passwordConfirmation = req.body.passwordConfirmation;
 
-    res.json({username, email});
+    // We can write like that it equals to const username = req.body.username
+    const {username, email, password, passwordConfirmation} = req.body;
+
+    if(!username || !email) {
+        return res.status(422).send({
+            errors: [{
+                title: 'Data missing!',
+                detail: 'please give email and password'
+            }]
+        });
+    }
+
+    if(password !== passwordConfirmation){
+        return res.status(422).send({
+            errors: [{
+                title: 'Invalid password!',
+                detail: 'Password and confirmartion should be same!'
+            }]
+        });
+    }
+
+    User.findOne({email: email}, function(err, existingUser){
+        if(err){
+            return res.status(422).send({
+                'mongoose' : 'Handle mongoose errors later'
+            });
+        }
+        if(existingUser){
+            return res.status(422).send({
+                'Invalid email!' : 'Email is already registered!'
+            });
+        }
+
+        const user = new User({
+            username,
+            email,
+            password
+        });
+
+        user.save(function(err){
+            if(err){
+                return res.status(422).send({
+                    'Error!' : err
+                });
+            }
+
+            return res.json({
+                'registered' :true
+            })
+        });
+    })
 }
--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const MIN_VALUE = 4;
 const MAX_VALUE = 32
 
 const userSchema = new Schema({
-    userName: {
+    username: {
         type:String,
         min: [MIN_VALUE, minValueError],
         max: [MAX_VALUE, maxValueError],
