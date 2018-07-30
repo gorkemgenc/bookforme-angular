2841c2e423e831eca704fc41468cc4ece59b33ce
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-31 01:06:07 +0300

    Changed auth and register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 import {Routes, RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
@@ -16,6 +18,8 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
+    FormsModule,
+    CommonModule
   ],
   providers: []
 })
