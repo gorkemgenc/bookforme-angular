ea995b19f38767ad6602e8e1585dc5671b74b03f
gorkem.genc <m.gorkemgenc@outlook.com>
2018-07-30 22:43:29 +0300

    Added Auth Middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -101,3 +101,36 @@ exports.register = function(req, res){
         });
     })
 }
+
+exports.authMiddleware = function(req, res, next){
+    const token = req.headers.authorization;
+
+    if(token){
+        const user = parseToken(token);
+
+        User.findById(user.userId, function(err, user) {
+            if(err){
+                return res.status(422).send({
+                    errors: normalizeErrors(err.errors)
+                });
+            }
+            if(user){
+                res.locals.user = user;
+                next();
+            }else{
+                return res.status(422).send({
+                    'Not authorized' : 'You need to login!'
+                });
+            }
+        });
+    }
+    else{
+        return res.status(422).send({
+            'Not authorized' : 'You need to login!'
+        });
+    }
+}
+
+function parseToken(token){
+    return jwt.verify(token.split(' ')[1], config.SECRET);
+}
