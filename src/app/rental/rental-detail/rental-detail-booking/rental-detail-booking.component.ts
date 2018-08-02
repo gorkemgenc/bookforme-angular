2ccb639a9d2796dfbaf260b7cbd1051273d7a9cd
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-02 05:36:09 +0300

    Code refactoring

--- src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Rental } from '../../shared/rental.model';
 
 @Component({
   selector: 'bookforme-rental-detail-booking',
@@ -8,33 +7,27 @@ import { Rental } from '../../shared/rental.model';
 })
 export class RentalDetailBookingComponent implements OnInit {
 
-  @Input() rental: Rental;
+  @Input() price: number;
+  daterange: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public daterange: any = {};
+  options: any = {
+      locale: { format: 'YYYY-MM-DD' },
+      alwaysShowCalendars: false,
+      opens: 'left'
+  };
 
-    public options: any = {
-        locale: { format: 'YYYY-MM-DD' },
-        alwaysShowCalendars: false,
-        opens: 'left'
-    };
+  selectedDate(value: any, datepicker?: any) {
 
-    public selectedDate(value: any, datepicker?: any) {
-        // this is the date the iser selected
-        console.log(value);
+      datepicker.start = value.start;
+      datepicker.end = value.end;
 
-        // any object can be passed to the selected event and it will be passed back here
-        datepicker.start = value.start;
-        datepicker.end = value.end;
-
-        // or manupulat your own internal property
-        this.daterange.start = value.start;
-        this.daterange.end = value.end;
-        this.daterange.label = value.label;
-    }
-
+      this.daterange.start = value.start;
+      this.daterange.end = value.end;
+      this.daterange.label = value.label;
+  }
 }
