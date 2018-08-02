c118731969cc9d8f7e67eadb65b2fe1a507ff632
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-02 05:27:53 +0300

    Rental-detail-cooking component

---
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Rental } from '../../shared/rental.model';
+
+@Component({
+  selector: 'bookforme-rental-detail-booking',
+  templateUrl: './rental-detail-booking.component.html',
+  styleUrls: ['./rental-detail-booking.component.scss']
+})
+export class RentalDetailBookingComponent implements OnInit {
+
+  @Input() rental: Rental;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public daterange: any = {};
+
+    public options: any = {
+        locale: { format: 'YYYY-MM-DD' },
+        alwaysShowCalendars: false,
+        opens: 'left'
+    };
+
+    public selectedDate(value: any, datepicker?: any) {
+        // this is the date the iser selected
+        console.log(value);
+
+        // any object can be passed to the selected event and it will be passed back here
+        datepicker.start = value.start;
+        datepicker.end = value.end;
+
+        // or manupulat your own internal property
+        this.daterange.start = value.start;
+        this.daterange.end = value.end;
+        this.daterange.label = value.label;
+    }
+
+}
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NgPipesModule } from 'ngx-pipes';
 import { MapModule } from '../common/map/map.module';
+import { Daterangepicker } from 'ng2-daterangepicker';
 
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
@@ -13,6 +14,7 @@ import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 import { AuthGuard } from '../auth/shared/auth.guard';
+import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
 
 const routes: Routes = [
     {path: 'rentals', component:RentalComponent,
@@ -28,14 +30,16 @@ const routes: Routes = [
         RentalListItemComponent,
         RentalComponent,
         RentalDetailComponent,
-        UppercasePipe
+        UppercasePipe,
+        RentalDetailBookingComponent
     ],
     imports:[
         CommonModule, 
         RouterModule.forChild(routes),
         HttpClientModule,
         NgPipesModule,
-        MapModule
+        MapModule,
+        Daterangepicker
     ],
     providers:[RentalService]
 
